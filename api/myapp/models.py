f201f63a2e625a758adfb92518778896fc2c6543
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-29 16:34:53 +0000

    User-skill relationships showing on through table

--- api/myapp/models.py
+++ api/myapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     biotext = models.TextField(max_length=300, default=None, null=True)
+    skills = models.ManyToManyField('Skill', related_name="user_list")
 
     def __str__(self):
         return self.username
@@ -13,7 +14,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.skill
-
-# class User_Skill(models.Model):
-#     user = models.ForeignKey('CustomUser', on_delete=models.PROTECT)
-#     skill = models.ForeignKey('Skill', on_delete=models.PROTECT)

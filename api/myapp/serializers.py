6f1e3dff61158c39f633e061740fdbb7cc669a55
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-29 14:57:28 +0000

    added biotext field to UserSerializer

--- api/myapp/serializers.py
+++ api/myapp/serializers.py
@@ -5,7 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'id', 'username', 'password', 'first_name', 'last_name') 
+        fields = ('email', 'id', 'username', 'password', 'first_name', 'last_name', 'biotext') 
         """ 
         only pull in the PROVIDED DJANGO USER FIELDS that are going to be used in creating a user, 
         and then add your extended fields,

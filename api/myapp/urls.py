4fac96250f42c0281868c4b52c4429b745783f73
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-12-06 15:09:55 +0000

    added url patterns for login; import jwt on urls

--- api/myapp/urls.py
+++ api/myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from .views import *
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
+
+
 
 router = routers.SimpleRouter()
 router.register(r'user', UserViewSet)
@@ -10,5 +13,8 @@ router.register(r'roles', RoleViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
